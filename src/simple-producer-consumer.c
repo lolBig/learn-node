7b19eea763435321f0ef5021dfbcf19e888a7143
chenximin <ximin.chen@rokid.com>
2017-11-01 17:15:20 +0800

    fixed deadlock

--- src/simple-producer-consumer.c
+++ src/simple-producer-consumer.c
@@ -6,26 +6,24 @@
 int isEnd = FALSE;
 int product_count = 0;
 int produce_times = 0;
-pthread_mutex_t mutex;
+pthread_mutex_t produce_mutex;
 pthread_cond_t consume_cond;
 pthread_cond_t produce_cond;
 
 void* consume(void *pVoid) {
   int pid = (int)pVoid;
   while (!isEnd || product_count > 0) {
-    sleep((unsigned int)(random() % 3));
+//    sleep((unsigned int)(random() % 3));
+    pthread_mutex_lock(&produce_mutex);
     if (product_count < 1) {
       RLOG("%d waiting for produce\n", pid);
-      pthread_mutex_lock(&mutex);
-      pthread_cond_wait(&consume_cond, &mutex);
-      pthread_mutex_unlock(&mutex);
+      pthread_cond_wait(&consume_cond, &produce_mutex);
       RLOG("%d continue consume\n", pid);
     }
-    pthread_mutex_lock(&mutex);
     --product_count;
-    pthread_mutex_unlock(&mutex);
     pthread_cond_signal(&produce_cond);
     RLOG("%d consume product left %d\n", pid, product_count);
+    pthread_mutex_unlock(&produce_mutex);
   }
   RLOG("%d consume finished\n", pid);
   pthread_exit(NULL);
@@ -34,31 +32,30 @@ void* consume(void *pVoid) {
 void* produce(void *pVoid) {
   int pid = (int)pVoid;
   while (!isEnd) {
-    sleep((unsigned int)(random() % 3));
+//    sleep((unsigned int)(random() % 3));
+    pthread_mutex_lock(&produce_mutex);
     if (product_count >= PRODUCT_MAX_COUNT) {
       RLOG("%d waiting for consume\n", pid);
-      pthread_mutex_lock(&mutex);
-      pthread_cond_wait(&produce_cond, &mutex);
-      pthread_mutex_unlock(&mutex);
+      pthread_cond_wait(&produce_cond, &produce_mutex);
       RLOG("%d continue produce\n", pid);
     }
-    pthread_mutex_lock(&mutex);
     ++product_count;
-    pthread_mutex_unlock(&mutex);
     pthread_cond_signal(&consume_cond);
     ++produce_times;
-    RLOG("%d produce product left %d total %d\n", pid, product_count, produce_times);
+    RLOG("%d produce product left %d total %d\n",
+         pid, product_count, produce_times);
     if (produce_times >= PRODUCE_MAX_TIMES) {
-      RLOG("%d produce finisehd\n", pid);
+      RLOG("%d produce finisehd left %d\n", pid, product_count);
       isEnd = TRUE;
     }
+    pthread_mutex_unlock(&produce_mutex);
   }
   pthread_exit(NULL);
 }
 
 int main (int argc, char *argv[]) {
   srandom((unsigned int)time(NULL));
-  pthread_mutex_init(&mutex, NULL);
+  pthread_mutex_init(&produce_mutex, NULL);
   pthread_cond_init(&consume_cond, NULL);
   pthread_cond_init(&produce_cond, NULL);
 
@@ -73,7 +70,7 @@ int main (int argc, char *argv[]) {
   pthread_join(pconsume, NULL);
   pthread_join(pproduce, NULL);
 
-  pthread_mutex_destroy(&mutex);
+  pthread_mutex_destroy(&produce_mutex);
   pthread_cond_destroy(&consume_cond);
   pthread_cond_destroy(&produce_cond);
   pthread_attr_destroy(&attr);
