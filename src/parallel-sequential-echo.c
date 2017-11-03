9fa658ea92313ed284820131cba62c3fe1457103
chenximin <ximin.chen@rokid.com>
2017-11-03 14:50:55 +0800

    fix deadlock

--- src/parallel-sequential-echo.c
+++ src/parallel-sequential-echo.c
@@ -1,34 +1,44 @@
 #include "learn_node.h"
 
-char letters[] = { '!', '@', '#', '$', '%', '^', '&', '*' };
+//signal is a better choice
+
+char letters[] = { '0', '1', '2', '3', '4', '5', '6', '7' };
 int echo_count = sizeof(letters);
-int echoTimes = 5;
-int isEnd = FALSE;
+int echo_times = 1;
 int *wait_flags = NULL;
 pthread_attr_t *attr = NULL;
 pthread_cond_t *conds = NULL;
 pthread_t *threads = NULL;
 pthread_mutex_t *mutex = NULL;
 
-void start() {
-  *wait_flags = FALSE;
-  pthread_cond_signal(conds);
-}
-
 void* echo(void *pvoid) {
   int i = (int)pvoid;
   int *wait_flag = wait_flags + i;
+  pthread_cond_t *cond = conds + i;
   RLOG("%d lock\n", i);
   pthread_mutex_lock(mutex);
   RLOG("%d get mutex\n", i);
-  while(*wait_flag) {
-    RLOG("%d waiting\n", i);
-    pthread_cond_wait(conds + i, mutex);
-    RLOG("%d wakeup\n", i);
-  }
-  if (i < echo_count - 1) {
-    *(wait_flag + 1) = FALSE;
-    pthread_cond_signal(conds + i + 1);
+  while(TRUE) {
+    while(*wait_flag) {
+      RLOG("%d waiting\n", i);
+      pthread_cond_wait(cond, mutex);
+      RLOG("%d wakeup\n", i);
+    }
+    if (i < echo_count - 1) {
+      *(wait_flag + 1) = FALSE;
+      pthread_cond_signal(cond + 1);
+    }
+    if (echo_times > 0) {
+      if (i >= echo_count - 1) {
+        --echo_times;
+        *wait_flags = FALSE;
+        pthread_cond_signal(conds);
+      }
+      RLOG("[%c]", letters[i]);
+      *wait_flag = TRUE;
+    } else {
+      break;
+    }
   }
   pthread_mutex_unlock(mutex);
   RLOG("%d finished\n", i);
@@ -56,7 +66,12 @@ int main(int argc, char **argv) {
     pthread_create(threads + i, attr, echo, (void*)i);
   }
 
-  start();
+  RLOG("start echo \n");
+  pthread_mutex_lock(mutex);
+  RLOG("start get mutex\n");
+  *wait_flags = FALSE;
+  pthread_cond_signal(conds);
+  pthread_mutex_unlock(mutex);
 
   for (int i = 0; i < echo_count; ++i) {
     pthread_join(*(threads + i), NULL);
